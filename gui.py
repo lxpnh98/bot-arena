83deed326326fb20b2f1ea7d61b81789f368523e
Alexandre Pinho <lxpnh98@gmail.com>
2017-11-18 22:30:26 +0000

    Add planning screen; possibility of random bot build.

--- gui.py
+++ gui.py
@@ -33,8 +33,8 @@ fontSub = pygame.font.SysFont("default", 20)
 
 class GUI(pgui.App):
     MAIN=0
-    PLANNING=0
-    PLAYING=1
+    PLANNING=1
+    PLAYING=2
     def __init__(self, screen_size):
         super().__init__()
         self.screen_size = (640, 480)
@@ -80,14 +80,31 @@ class GUI(pgui.App):
 
         # Other gui elements
         # Normal Buttons
-        new_game_button = pgui.Button("New game")
-        new_game_button.connect(pgui.CLICK, self.new_game, ())
-        layout.add(new_game_button, 20, 40)
+
+        if self.screen_state == GUI.MAIN:
+            play_button = pgui.Button("Plan")
+            play_button.connect(pgui.CLICK, self.plan_screen, None)
+            layout.add(play_button, 20, 100)
+
+        if self.screen_state == GUI.PLANNING:
+            buy_chasis_button = pgui.Button("Buy Chasis")
+            buy_chasis_button.connect(pgui.CLICK, self.plan_screen, None)
+            layout.add(buy_chasis_button, 20, 100)
+            buy_body_button = pgui.Button("Buy Body")
+            buy_body_button.connect(pgui.CLICK, self.plan_screen, None)
+            layout.add(buy_body_button, 150, 100)
+
+            new_game_button = pgui.Button("New game")
+            new_game_button.connect(pgui.CLICK, self.new_game, None)
+            layout.add(new_game_button, 20, 40)
 
         if self.screen_state == GUI.PLAYING:
             main_menu_button = pgui.Button("Main menu")
-            main_menu_button.connect(pgui.CLICK, self.main_menu, ())
+            main_menu_button.connect(pgui.CLICK, self.main_menu, None)
             layout.add(main_menu_button, 20, 70)
+            play_button = pgui.Button("Plan")
+            play_button.connect(pgui.CLICK, self.plan_screen, None)
+            layout.add(play_button, 20, 100)
 
         self.init(layout)
 
@@ -104,45 +121,26 @@ class GUI(pgui.App):
     def new_game(self, *args):
         self.screen_state = GUI.PLAYING
 
-        if not self.campaign:
-            p1 = player.HumanPlayer(None)
-            p2 = player.Player(None)
-            p3 = player.Player(None)
-            p4 = player.Player(None)
-            w = world.World(Vector(*self.screen_size))
-            self.campaign = campaign.Campaign(p1, [level.Level(w, 10)])
+        self.campaign.player.bots = []
+        p1 = self.campaign.player
+        p2 = player.Player(None)
+        p3 = player.Player(None)
+        p4 = player.Player(None)
+        w = world.World(Vector(*self.screen_size))
+        self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
+        self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
+        #self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
+        #self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.ULTRA_TANK)
+        self.add_bot(p1, self.screen_size, (255, 0, 0))
 
-            self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
-            self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
-            #self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
-            #self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.ULTRA_TANK)
-
-            self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.LIGHT)
-            #self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.LIGHT)
-            #self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.ULTRA_LIGHT)
-            self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.TANK)
-            w.addPlayer(p1)
-            w.addPlayer(p2)
-
-        else:
-            self.campaign.player.bots = []
-            p1 = self.campaign.player
-            p2 = player.Player(None)
-            p3 = player.Player(None)
-            p4 = player.Player(None)
-            w = world.World(Vector(*self.screen_size))
-            self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
-            self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
-            #self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.NORMAL)
-            #self.add_bot(p1, self.screen_size, (255, 0, 0), build=Build.ULTRA_TANK)
-
-            self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.LIGHT)
-            #self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.LIGHT)
-            #self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.ULTRA_LIGHT)
-            self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.TANK)
-            w.addPlayer(p1)
-            w.addPlayer(p2)
-            self.campaign.levels = [level.Level(w, 10)]
+        self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.LIGHT)
+        #self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.LIGHT)
+        #self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.ULTRA_LIGHT)
+        self.add_bot(p2, self.screen_size, (0, 255, 0), build=Build.TANK)
+        self.add_bot(p2, self.screen_size, (0, 255, 0))
+        w.addPlayer(p1)
+        w.addPlayer(p2)
+        self.campaign.levels = [level.Level(w, 10)]
 
         self.campaign.play_level(self.campaign.levels[0])
         self.place_ui()
@@ -152,32 +150,41 @@ class GUI(pgui.App):
         self.campaign = None
         self.place_ui()
 
-    def add_bot(self, player, screen_size, bot_color, build=0):
+    def plan_screen(self, *args):
+        self.screen_state = GUI.PLANNING
+        if self.campaign == None:
+            w = world.World(Vector(*self.screen_size))
+            self.campaign = campaign.Campaign(player.HumanPlayer(None), [level.Level(w, 10)])
+        self.place_ui()
+
+    def add_bot(self, player, screen_size, bot_color, build=-1):
+        if build == -1:
+            build = random.randint(Build.ULTRA_LIGHT, Build.ULTRA_TANK)
         if build == Build.ULTRA_LIGHT:
             b = bot.Bot(Vector(random.random(), random.random()).dot(Vector(*screen_size)),
                         components.Chasis(None, 5, None),
                         color=bot_color)
             b.chasis.addBody(components.Body(None, size=7, weight=4, hp=4))
             b.chasis.body.addWeapon(components.Weapon(None, bullet_size=2, bullet_speed=300, bullet_damage=0.05, reload_time=0.1))
-        if build == Build.LIGHT:
+        elif build == Build.LIGHT:
             b = bot.Bot(Vector(random.random(), random.random()).dot(Vector(*screen_size)),
                         components.Chasis(None, 5, None),
                         color=bot_color)
             b.chasis.addBody(components.Body(None, size=10, weight=7, hp=7))
             b.chasis.body.addWeapon(components.Weapon(None, bullet_size=3, bullet_speed=140, bullet_damage=0.2, reload_time=0.3))
-        if build == Build.NORMAL:
+        elif build == Build.NORMAL:
             b = bot.Bot(Vector(random.random(), random.random()).dot(Vector(*screen_size)),
                         components.Chasis(None, 5, None),
                         color=bot_color)
             b.chasis.addBody(components.Body(None, size=17, weight=10, hp=10))
             b.chasis.body.addWeapon(components.Weapon(None, bullet_size=4, bullet_speed=110, bullet_damage=1, reload_time=1))
-        if build == Build.TANK:
+        elif build == Build.TANK:
             b = bot.Bot(Vector(random.random(), random.random()).dot(Vector(*screen_size)),
                         components.Chasis(None, 5, None),
                         color=bot_color)
             b.chasis.addBody(components.Body(None, size=30, weight=15, hp=17.5))
             b.chasis.body.addWeapon(components.Weapon(None, bullet_size=6, bullet_speed=70, bullet_damage=3.25, reload_time=2.5))
-        if build == Build.ULTRA_TANK:
+        elif build == Build.ULTRA_TANK:
             b = bot.Bot(Vector(random.random(), random.random()).dot(Vector(*screen_size)),
                         components.Chasis(None, 5, None),
                         color=bot_color)
